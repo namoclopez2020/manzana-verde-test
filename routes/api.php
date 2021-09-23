6ff11752b79c9c139a44cbd7fec5ed85c3750a38
namoclopez2020 <62824677+namoclopez2020@users.noreply.github.com>
2021-09-23 13:08:39 -0500

    Autentication finished

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login',  [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('register', [App\Http\Controllers\AuthController::class, 'register']);
+    Route::get('logout', [App\Http\Controllers\AuthController::class, 'logout']);
 });
+
